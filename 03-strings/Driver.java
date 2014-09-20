c06b3bb265ac78c479c342e24eeaccbd0906f61a
Daisy <dbarbanel555@gmail.com>
2014-09-20 09:40:15 -0400

    completed hw 3 on strings and added to github

---
+++ 03-strings/Driver.java
@@ -0,0 +1,13 @@
+//HW 3, due 9/22/14, by Daisy Barbanel
+
+
+public class Driver {
+    public static void main(String[] args){
+	String s ="daisy barbanel";
+	int Space = s.indexOf(' ');
+	String first = s.substring(0,Space);
+	String last = s.substring(Space + 1);
+	//System.out.println(first);
+	//System.out.println(last);
+    }
+}
