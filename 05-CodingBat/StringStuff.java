14a590e12d18ed1ac8398e2074ca9db59daab743
daisy <daisybarbanel@yahoo.com>
2014-09-23 21:15:46 -0400

    Completed hw 5 on Tues 9/23/14

---
+++ 05-CodingBat/StringStuff.java
@@ -0,0 +1,28 @@
+//hw 5, 9/24/14
+
+public class StringStuff {
+	
+//nonstart
+	public String nonStart(String a, String b) {
+  		String newA = a.substring(1);
+  		String newB = b.substring(1);
+  		return newA + newB;
+	}
+
+//makeABBA
+	public String makeAbba(String a, String b) {
+   		return a + b + b + a;
+  
+	}
+
+//firstHalf
+	public int diff21(int n) {
+   		int absDiff = Math.abs(n - 21);
+   		if (n <= 21) {
+      			return absDiff;
+   		} else {
+     			 return 2 * absDiff;
+      		}
+	}
+
+}
