375691c4ab1f5c31d818fb630ebf3ad9ade5527a
daisyb <daisybarbanel@yahoo.com>
2014-09-24 17:14:24 -0400

    completed class codingbat assignments Wens 9/24/14

--- 05-CodingBat/StringStuff.java
+++ 05-CodingBat/StringStuff.java
@@ -1,4 +1,4 @@
-//hw 5, 9/24/14
+//worked in class with Fanny Ma
 
 public class StringStuff {
 	
@@ -25,5 +25,45 @@ public class StringStuff {
       		}
 	}
 
+
+/* Sorry Fanny I accidentally didn't add the file from class and homers not working so I rewrote them */
+
+//working
+    public boolean nearHundred(int n) {
+        if ((90 <= n && n <= 110) || (190 <= n && n<= 210)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+//working
+    public boolean mixStart(String str) {
+        if (str.length() < 1) return false;
+            return str.substring(1).startsWith("ix");
+    }  
+    
+//working
+    public int teaParty(int tea, int candy) {
+       if (tea < 5 || candy < 5) {
+            return 0;
+        } else if (tea*2 <=  candy || candy*2 <= tea) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+    
+//working
+    public boolean lastDigit(int a, int b, int c) {
+        int newA = a % 10;
+        int newB = b % 10;
+        int newC = c % 10;
+        if (newA == newB || newA == newC || newB == newC) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
  
