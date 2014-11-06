d474134b8d30aef9bd19a6db27f84b4b729a690c
Daisy B <daisybarbanel@yahoo.com>
2014-11-06 08:42:20 -0500

    classwork

--- 14-Arraystuff/Arraystuff.java
+++ 14-Arraystuff/Arraystuff.java
@@ -3,21 +3,18 @@ import java.util.*;
 public class Arraystuff {
     private int[] a;
     private Random rnd;
-    /* by making a variable final we can't change
-       the value after the initial asssignment 
-    */
-    public final int final_example = 123;
+
 
     public Arraystuff(int n){
 	rnd = new Random();
 	a = new int[n];
 	for (int i=0;i<a.length;i++){
-	    a[i] = 75+rnd.nextInt(76);
+	    a[i] = 0+rnd.nextInt(21);
 	}
     }
     
     public Arraystuff() {
-	    this(100);
+	    this(50000000);
     }
 
     public String toString(){
@@ -48,6 +45,16 @@ public class Arraystuff {
 	return n;
     }
 
+    public int maxVal(int[] a){
+	int n = a[0];
+	for (int i =0; i <a.length; i++){
+	    if (a[i] > n){
+		n = a[i];
+	    }
+	}
+	return n;
+    }
+
     public int freq(int index){
 	int n = a[index];
 	int count = 0;
@@ -59,13 +66,76 @@ public class Arraystuff {
 	return count;
     }
 
+    public int freqNum(int n){
+        int count = 0;
+	for (int i = 0;i < a.length;i++){
+	    if (a[i] == n){
+		count += 1;
+	    }
+	}
+	return count;
+    }
+
+
+    public int mode(int[] a){
+	int count = 0;
+	int mode = a[0];
+	for (int i =0;i<a.length;i++){
+	    if (freq(i) > count){
+		count = freq(i);
+		mode = a[i];
+	    }
+	}
+	System.out.println(mode);
+	return count;
+    }
+
+    public int betterMode(int[] a){
+	int[] freqArray = new int[maxVal(a) + 1];
+	for(int i = 0;i<a.length;i++){
+	    int n = a[i];
+	    freqArray[n] += 1;
+	}
+	int modeCount =maxVal(freqArray);
+	for (int i =0; i<freqArray.length;i++){
+	    if (freqArray[i] == modeCount){
+		System.out.println("mode:" + i);
+	    }
+	}
+	return modeCount;
+    }
+
     public static void main(String[] args) {
-	Arraystuff as = new Arraystuff();
-	System.out.println(as);
+	int n= 100;
+	if (args.length > 0){
+	    n = Integer.parseInt(args[0]);
+	}
+	
+	Arraystuff as = new Arraystuff(n);
+	int modeType = 1;
+	if (args.length > 1){
+	    modeType = Integer.parseInt(args[1]);
+        }
+	    
+	if (modeType == 1){
+	    System.out.println("fast mode");
+	    System.out.println(as.betterMode(as.a));
+	}
+	if (modeType == 2){
+	    System.out.println("slow mode");
+	    System.out.println(as.mode(as.a));
+	}
+	/*
+
 	System.out.println();
 	System.out.println(as.find(100));
 	System.out.println(as.maxVal());  
 	System.out.println(as.freq(2));	
-	System.out.println(2%10);
+	System.out.println();
+	*/
+	//System.out.println(as);
+	//System.out.println(as.mode(as.a));
+	//System.out.println("other mode method");
+	//System.out.println(as.betterMode(as.a));
     }
 }
