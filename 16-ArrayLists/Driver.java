d002eaa8aa65f6a621f29ef2659e58e1746d2a6a
Daisy B <daisybarbanel@yahoo.com>
2014-11-12 08:42:33 -0500

    started arraylists

---
+++ 16-ArrayLists/Driver.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Driver{
+    public static void removeDupes(ArrayList ai){
+	for(int i = 0; i < ai.size();i++){
+	}
+    }
+    public static void main(String[] args){
+	ArrayList<int> ai = new ArrayList<int>();
+	
+    }
+}
