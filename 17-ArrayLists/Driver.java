7316fdacb7d310405e0be8b0d1ad591c4925a87a
Daisy <dbarbanel555@gmail.com>
2014-11-14 19:39:01 -0500

    compled hw 17 on fri 11/14

--- 17-ArrayLists/Driver.java
+++ 17-ArrayLists/Driver.java
@@ -2,43 +2,75 @@ import java.util.*;
 
 public class Driver{
     public static void main(String[] args){
-	 Random r = new Random();
-	//ARRAYLISTS
-	ArrayList<Integer> al = new ArrayList<Integer>();
-	System.out.println(al);
-	for(int i=0;i<10000;i++){
-	    al.add(i);
+	Random r = new Random();
+	int len = 7;
+	
+	if(args.length == 0){
+	    System.out.println("put in args: choose al(arrayList) or a(array)");
+	    System.exit(0);
 	}
-	System.out.println(al);
-
-	ArrayList<Integer> ret = new ArrayList<Integer>();
-	System.out.println(ret);
-
-	while (al.size()>0){
-	    ret.add(al.remove(  r.nextInt(al.size()))  );
+	String choice = args[0];
+	if (args.length > 1){
+	    len = Integer.parseInt(args[1]);
 	}
-	System.out.println(ret);
-    
+	
+	if(choice.equals("al")){
 
-	//ARRAYS
+	    //ARRAYLISTS
+	    ArrayList<Integer> al = new ArrayList<Integer>();
+	    for(int i=0;i<len;i++){
+		al.add(i);
+	    }
+	    //   System.out.println(al);
 
+	    ArrayList<Integer> ret = new ArrayList<Integer>();
+	    while (al.size()>0){
+		ret.add(al.remove( r.nextInt(al.size()))  );
+	    }
+	    al = ret;
+	    //	    System.out.println(al);
+	} else {    
 
-	//working on it :(
-        int[] a = new int[7];
-	for (int i=0;i<7;i++){
-	    a[i]=i;
+	    //ARRAYS
+	    int[] a = new int[len];
+	    for (int i=0;i<a.length;i++){
+	        a[i]=i;
+	    }
+
+	    int[] newA = new int[a.length];
+	    boolean used;
+	    for(int i = 0; i<a.length;i++){
+	        used = false;
+	        while(used == false){
+		    int index = r.nextInt(a.length);
+		    if (a[index] != -1){
+			newA[i] = a[index];
+			a[index] = -1;
+			used = true;
+		    }
+	        }
+	    }
+
+	    a = newA;
+
+	    // for (int i=0;i<a.length;i++){
+	    //     System.out.println(i + ": " +a[i]);
+	    // }
 	}
-	int[] newArray = new int[a.length];
-	int size = a.length -1;
-	for(int i=0;i<a.length;i++){
-	    int ran = r.nextInt(size);
-	    newArray[ran] = i;
-	    
 
-	}
+	/*
+	  The ArrayList method takes longer than 
+	  the one I wrote for Arrays.
+	  Up until lengths in the 10,000's the difference is
+	  barely noticible, however:	  
+	  At length 100,000  ArrayList time = 1.12s,
+	                     Array     time = 0.16s
+	  By length 300,000 the ArrayList time is close to 10s,
+	  While the Array time does not reach 10s until
+	  length 7,000,000.
 
-	for (int i=0;i<newArray.length;i++){
-	    System.out.println(i + ": " +newArray[i]);
-	}
+	  space wise they're probably similar since both are storing two arrays of the same
+	  length, which isn't optimal, but doesn't seem all that bad either.
+        /*
     }   
 }
