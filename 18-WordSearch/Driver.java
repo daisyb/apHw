c061a5002890334b51c101c067ea9b6590803239
daisy <daisybarbanel@yahoo.com>
2014-11-19 21:33:34 -0500

    did the first part of word search

--- 18-WordSearch/Driver.java
+++ 18-WordSearch/Driver.java
@@ -1,7 +1,20 @@
 public class Driver {
 
-		public static void main(String[] args) {
-				WordSearch w = new WordSearch();
-				System.out.println(w);
-		}
+    public static void main(String[] args) {
+	WordSearch w = new WordSearch();
+	System.out.println(w);
+	w.addWordHB("hello",1,4);
+	System.out.println(w);
+	w.addWordHB("look",1,3);
+	w.addWordHF("a",3,8);
+	w.addWordHF("a",2,8);
+	w.addWordHF("rabbit",3,6);
+	w.addWordHB("bannana",5,20);
+	// System.out.println(w.overlap("look",3,6));
+	// w.addWordH("look",3,5);
+	// w.addWordH("hello",100,5);
+	// w.addWordH("hello",30,555);
+				
+	System.out.println(w);
+    }
 }
