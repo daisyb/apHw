e73b1bf788469c98401abb3ee72fefabb5feb2fd
Daisy <dbarbanel555@gmail.com>
2014-11-20 19:11:59 -0500

    did the vertical words

--- 18-WordSearch/Driver.java
+++ 18-WordSearch/Driver.java
@@ -2,16 +2,14 @@ public class Driver {
 
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
+	w.addWordHF("hello",1,3);
 	System.out.println(w);
-	w.addWordHB("hello",1,4);
-	System.out.println(w);
-	w.addWordHB("look",1,3);
+	w.addWordHF("look",1,2);
 	w.addWordHF("a",3,8);
-	w.addWordHF("a",2,8);
 	w.addWordHF("rabbit",3,6);
 	w.addWordHB("bannana",5,20);
-	// System.out.println(w.overlap("look",3,6));
-	// w.addWordH("look",3,5);
+	w.addWordVB("table",4,5);
+	w.addWordVF("plant",15,3);
 	// w.addWordH("hello",100,5);
 	// w.addWordH("hello",30,555);
 				
--- 18-WordSearch/WordSearch.java
+++ 18-WordSearch/WordSearch.java
@@ -16,7 +16,7 @@ public class WordSearch{
 				
     }
     public WordSearch(){
-	this(10,15);
+	this(10,20);
     }
 
     public String toString(){
@@ -31,14 +31,14 @@ public class WordSearch{
     }
 
 
-    public void addWordHF(String w,int row, int col){
+    public void addWord(String w,int row, int col, boolean forward, boolean horizontal){
 	int r = row, c = col;
-	//keeps word from going over
 	if ((c + w.length()) > board[r].length){
 	    c = c - ((c + w.length()) - board[r].length);
 	}
+
 	col = c;
-	Boolean overlap = false;
+
 	int slen = w.length();
 	for (int i=0; i < slen;i++){
 	   
@@ -50,11 +50,17 @@ public class WordSearch{
 	    
 		char current = board[r][c];
 		int index = 0;
+			Boolean overlap = false;
 		for(int j = 0;j<w.length();j++){
 		     if (w.charAt(j) == current){
-	       		overlap = true;
-			index = j;
-			current = board[r][c +1];
+			 if (overlap){
+			     c+=1;
+			     current = board[r][c + 1];
+			 } else {
+			     overlap = true;
+			     current = board[r][c + 1];
+			 }
+			 index = j;
 		     } else if (overlap && current != '.'){
 		     	 overlap = false;
 		     } 
@@ -64,71 +70,66 @@ public class WordSearch{
 		col = c - index; 
 	    	break;
 	     }
-	    }
-	    c +=1;
-	}
-	c = col;
-
-	for (int i=0;i<w.length();i++){
-	    board[r][c] = w.charAt(i);
-	    c++;
-	}
-    }
-
-    public void addWordHB(String w,int row, int col){
-	int r = row, c = col;
-	//keeps word from going over
-	if ((c + w.length()) > board[r].length){
-	    c = c - ((c + w.length()) - board[r].length);
-	}
-	col = c;
-	Boolean overlap = false;
-	int slen = w.length();
-	for (int i=0; i < slen;i++){
-	   
-	    if (board[r][c] != '.'){
-		if (col <= c){
-		    col = c + 1;
-		    slen +=1;
-		}
 	    
-		char current = board[r][c];
-		int index = 0;
-		for(int j = 0;j<w.length();j++){
-		     if (w.charAt(j) == current){
-	       		overlap = true;
-			index = j;
-			current = board[r][c +1];
-		     } else if (overlap && current != '.'){
-		     	 overlap = false;
-		     } 
-		}
-	    if (overlap){
-		board[r][c] = '.';
-		col = c - index; 
-	    	break;
-	     }
 	    }
 	    c +=1;
 	}
 	c = col;
-
-	for (int i=w.length() - 1;i>=0;i--){
-	    board[r][c] = w.charAt(i);
-	    c++;
+	if (c < 0){
+	    c +=1;
+	}
+	if (horizontal){
+	    if (forward){
+		for (int i=0;i<w.length();i++){
+		    board[r][c] = w.charAt(i);
+		    c++;
+		}
+	    } else{
+		for (int i=w.length() - 1;i>=0;i--){
+		    board[r][c] = w.charAt(i);
+		    c++;
+		}
+	    }
+	} else{
+	    if (forward){
+		for (int i =0;i<w.length();i++){
+		    board[r][c] = w.charAt(i);
+		    r++;
+		}
+	    } else {
+		System.out.println(c);
+		System.out.println(r);
+		for (int i = w.length() -1;i >=0;i--){
+		    board[r][c] = w.charAt(i);
+		    r++;
+		}
+	    }
 	}
     }
+ 
+   public void addWordHF(String w,int row, int col){
+	addWord(w,row,col,true,true);
+    }
+    public void addWordHB(String w, int row, int col){
+	addWord(w,row,col,false,true);
+    }
+    public void addWordVF(String w, int row,int col){
+	addWord(w,col,row,true,false);
+    }
+    public void addWordVB(String w, int row, int col){
+	addWord(w,col,row,false,false);
+    }
+   
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
-	w.addWordHB("hello",1,4);
+	w.addWordHF("hello",1,3);
 	System.out.println(w);
-	w.addWordHB("look",1,3);
+	w.addWordHF("look",1,2);
 	w.addWordHF("a",3,8);
-	w.addWordHF("a",2,8);
 	w.addWordHF("rabbit",3,6);
 	w.addWordHB("bannana",5,20);
-	// System.out.println(w.overlap("look",3,6));
-	// w.addWordH("look",3,5);
+	w.addWordVB("table",4,5);
+	w.addWordVF("plant",15,3);
 	// w.addWordH("hello",100,5);
 	// w.addWordH("hello",30,555);
 				
