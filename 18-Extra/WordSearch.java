1c69746b781d2fdfa5998b7e2d8f6a9ee1e55be0
daisy <daisybarbanel@yahoo.com>
2014-11-29 15:41:30 -0500

    overlaps work for all types and directions

--- 18-Extra/WordSearch.java
+++ 18-Extra/WordSearch.java
@@ -1,3 +1,9 @@
+/*
+  This version of the word search makes overlaps much more likely.
+  Most changes to original were done in the addBetter and addWord2 methods.
+*/
+
+
 import java.util.*;
 import java.io.*;
 
@@ -45,7 +51,7 @@ public class WordSearch{
 	    int tries = 0;
 	    boolean worked = false;
 	    while (tries < 10 && !worked){
-		worked = addWord(word);
+		worked = addWord2(word);
 		tries +=1;
 	    }
 	    if (worked){
@@ -81,6 +87,7 @@ public class WordSearch{
 	}
 	return s;
     }
+
     public Boolean addWord(String w){
 	r = new Random();
        	int row = r.nextInt(board.length);
@@ -139,7 +146,7 @@ public class WordSearch{
 	return success;
     }
 
-	
+   
     public void fillGrid(){
 	r = new Random();
 	for(int i = 0; i < board.length;i++){
@@ -184,38 +191,72 @@ public class WordSearch{
 	}
     }
 
-    public boolean overBoard(int length,int row, int col, boolean horizontal){
-	if (col + length < board[0].length && col  > 0 && horizontal){
+    //modified addWord for use with addWordBetter
+    public Boolean addWord2(String w){
+	r = new Random();
+	boolean success = true;
+	int type = r.nextInt(3);
+	boolean forward = false;
+	if (r.nextInt(2) == 0){
+	    forward = true;
+	}
+	try{
+	    addWordBetter(w,forward,type);
+	} catch (Exception UnsupportedOperation){
+	    success = false;
+	}		    
+	return success;
+    }
+
+    //returns true if the word is within bounds, false if not
+    //used in addWordBetter
+    public boolean overBoard(int length,int row, int col){
+	if (col + length < board[0].length && col  > 0){
 	    return true;
-	} else if (row + length < board.length && row > 0 && !horizontal){
+	} else if (row + length < board.length && row > 0){
 	    return true;
 	} else {
 	    return false;
 	}
     }
-    public void addWordBetter(String w, int row, int col,boolean forward, boolean horizontal){
-        int r  = row, c = col;
+
+    //makes overlaps more likely, one function for all add types
+    public void addWordBetter(String w, boolean forward, int type){
+       	Random rnd = new Random();                                 
+	int r  = 0, c = 0;
 	int rAdd = 0, cAdd =0;
         boolean overlap = false;
 	int index = 0;
 	if(!forward){
 	    w = new StringBuilder(w).reverse().toString();
 	}
-	if (horizontal){
+
+	if (type == 0){ //horizontal
 	    cAdd = 1;
-	} else {
+	} else if (type == 1){ //vertical
+	    rAdd = 1;
+	} else if (rnd.nextInt(2) == 0){ //Diagonal right
+	    cAdd = 1;
+	    rAdd = 1;
+	} else {  //Diagonal left;
+	    cAdd = -1;
 	    rAdd = 1;
 	}
+
 	for(int i =0;i<board.length;i++){
 	    for(int j =0;j<board[r].length;j++){
 		char current = board[i][j];
 		for(int k=0;k<w.length();k++){
-		    if (overBoard(w.length(), i - k  , j - k , horizontal)){
+		    if (overBoard(w.length(), i - k  , j - k)){
                         if(current != '.'){
                             if(w.charAt(k)==current){
 				if(overlap){
-				    if (horizontal) j +=1;
-				    else i +=1;
+				    if (type == 0) j +=1;
+				    else if (type == 1) i +=1;
+				    else { 
+					i+= rAdd; 
+					j+= cAdd;
+				    }
 				}
                                 overlap = true;
 				index = k;
@@ -232,30 +273,45 @@ public class WordSearch{
 		    }
 		}
 		if (overlap){
-		    if (horizontal){
+		    if (type == 0){
 			r = i;
 			c = j - index;
 			break;
-		    } else {
+		    } else if (type == 1){
 			r = i -index;
 			c = j;
+			break;
+		    } else {
+			r = i - index;
+			c = j - index;
 			break;
 		    }
 		}
 	    }
+
 	    if (overlap){
 		break;
 	    }
                
 	}
-        System.out.println("row: " + r + " col:" + c);
-        if (horizontal){
+	if (!overlap){
+	    r = rnd.nextInt(board.length);
+	    c = rnd.nextInt(board[r].length);
+	}
+        if (type == 0){
 	    addWordH(w,r,c,true);
-	}else {
+	}else if (type == 1){
 	    addWordV(w,r,c,true);
+	} else {
+	    if (cAdd > 0){
+		addWordDRF(w,r,c);
+	    } else {
+		addWordDLF(w,r,c);
+	    }
 	}
 
     }
+
     public void addWordH(String w, int row, int col, boolean forward){
 	int r = row, c = col;
 	if ((c + w.length()) > board[r].length){
@@ -426,25 +482,6 @@ public class WordSearch{
   
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
-	//w.addWordH("hello",1,3,true);
-	//System.out.println(w);
-	//w.addWordH("look",1,0,true);
-	//w.addWordH("a",3,8,true);
-	//w.addWordH("rabbit",3,7,true);
-	//w.addWordH("bannana",5,20,false);
-	//w.addWordV("table",0,20, false);
-	//w.addWordV("pool",20,20, true);
-	//w.addWordH("hello",100,5);
-	//w.addWordH("hello",30,555);
-				
-	//w.addWordDRB("soup",0,0);
-	// System.out.println(w.addWord("hello"));
-	// System.out.println(w.addWord("look"));
-	// System.out.println(w.addWord("pool"));
-	// System.out.println(w.addWord("school"));
-
-	// System.out.println(w);
-	// w.fillGrid();
         w.buildPuzzle(10);
 
 	System.out.println(w);
